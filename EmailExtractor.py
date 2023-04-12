57ca29b32fbf8452513ce2719f1bb9f07742d2b0
Krlyn <karolina.janiak@student.wat.edu.pl>
2023-04-13 01:06:29 +0200

    extractor cd

--- EmailExtractor.py
+++ EmailExtractor.py
@@ -1,38 +1,19 @@
 #Zadanie 1 - MNGKW - Regex - Karolina Janiak - WCY19KA1S0
-# import re
-# class EmailExtractor:
-#         email = input("Podaj swój email:")
-#        # def __init__(self):
-#         pattern = r'^(\w+)\.(\w+)@(\w+)\.(\w+\.\w+\.\w+)$'
-#         domain = email.split('@')[1]
-#
-#         match = re.match(pattern, email)
-#
-#
-#         if match:
-#                 name = match.group(1)
-#                 surname = match.group(2)
-#                 status = match.group(3)
-#                 domain = 'student'
-#                 print("Name:", name)
-#                 print("Surname:", surname)
-#                 print("Status:", status)
-#                 print("Domain:", domain)
-#
-#
+
 import re
 
 class EmailExtractor:
+
+
     def __init__(self, email):
         self.email = email
         self.status = self.is_student()
         self.name = self.get_name()
         self.surname = self.get_surname()
-        self.sex = self.is_male()
+        self.gender = self.is_male()
 
     def is_student(self):
-        pattern = r'^(\w+)\.(\w+)@(\w+)\.(\w+\.\w+\.\w+)$'
-        pattern = r".+@student.wat.edu.pl"
+        pattern = r'(\w+)\.(\w+)@student.wat.edu.pl'
         return bool(re.match(pattern, self.email))
 
     def get_name(self):
@@ -40,19 +21,19 @@ class EmailExtractor:
         match = re.match(pattern, email)
         if match:
                 name = match.group(1)
-                return name.capitalize() in self.name
+                return name.capitalize()
         else:
-                return "Have you got name? Try again"
+                return ValueError('Have you got name? Try again')
 
 
     def is_male(self):
-        pattern = r'^(a$)\.(\w+)@(\w+)\.(\w+\.\w+\.\w+)$'
+        pattern = r'^(a$)\.(\w+)@(\w+)\.wat.edu.pl'
         match = re.match(pattern, email)
         if match:
                 name = match.group(1)
-                return "Female" in self.sex
+                print('female')
         else:
-                return "Male" in self.sex
+                print('male')
 
 
 
@@ -61,15 +42,15 @@ class EmailExtractor:
         match = re.match(pattern, email)
         if match:
                 surname = match.group(2)
-                return surname.capitalize() in self.surname
+                return surname.capitalize()
         else:
+                return ValueError('Have you got surname? Try again')
+        #surname = re.sub(pattern, '', self.email)
 
-        #surname = re.sub(pattern, "", self.email)
 
-
-email = input("Podaj email:")
+email = input('Podaj email:')
 ext = EmailExtractor(email)
-print("Name:", ext.name)
-print("Surname", ext.surname)
-print("Status:", ext.status)
-print("Sex:", ext.sex)
+print('Name:', ext.name)
+print('Surname', ext.surname)
+print('Student:', ext.status)
+print('Gender:', ext.gender)
